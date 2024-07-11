fabfb9c1156598621c02b0f288aa7c303d2affb5
vaibhav8862 <vaibhavsonawane991@gmail.com>
2024-07-11 14:42:03 +0530

    first commit

---
+++ let/if.js
@@ -0,0 +1,22 @@
+
+// for (let a=2;a<=10;a++){
+// console.log("mul of table"+a);
+// for (let b=1;b<=10;b++){
+//     const table=a*b;
+//     console.log(table);
+// }
+// }
+
+// const a=5;
+// for(let i=1;i<=a;i++){
+//     let line="";
+//     for (let j=1;j<=i;j++){
+//         line +=j;
+//     }
+//     console.log(line);
+// }
+
+console.log(10+20);
+
+console.log(1==1);
+console.log('1'==true);
